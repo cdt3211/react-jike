835a139ba7cb8b4e83af7a9737fb0b66f6505ba3
Abner <1228428816@qq.com>
2024-08-12 14:23:58 +0800

    封装token操作

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,10 @@
 // 统一导出工具函数
 import { request } from "./request";
+import { setToken, getToken, removeToken } from "./token";
 
-export { request };
+export {
+  request,
+  setToken,
+  getToken,
+  removeToken
+};
